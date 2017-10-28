749cbbf3d44ca8e8c323481f776abc2af0497b24
Manish Devgan <manish.nsit8@gmail.com>
2017-10-28 19:17:30 +0100

    Removed Comments

--- src/responses.js
+++ src/responses.js
@@ -4,34 +4,6 @@ const Table = require('cli-table');
 const chalk = require('chalk');
 
 
-
-function TableReponse(data,metaData,callback){
-	var cols = data[data.length - 1].columns;
-	var headers = [];
-	var keys = Object.keys(cols);
-	keys.map((key)=>{
-		headers.push(cols[key]);
-	})
-	var table = new Table({
-		head : headers,
-		colWidths : [20,5,20]
-	})
-	for(var i = 0; i < metaData.length ; i++){
-		var content = [];
-		for(var j = 0; j < keys.length ; j ++){
-			content.push(metaData[i][keys[j]]);
-		}
-		table.push(content);
-	}
-	callback(table);
-}
-
-function AnswerResponse(data,callback){
-	var answer = data[0].expression;
-	callback(answer);
-}
-
-
 module.exports = {
 	TableReponse : function(data,metaData,callback){
 					var cols = data[data.length - 1].columns;
--- src/susi.js
+++ src/susi.js
@@ -16,32 +16,6 @@ function callSUSI(req,callback){
 	});
 }
 
-/*function TableReponse(data,metaData,callback){
-	var cols = data[data.length - 1].columns;
-	var headers = [];
-	var keys = Object.keys(cols);
-	keys.map((key)=>{
-		headers.push(cols[key]);
-	})
-	var table = new Table({
-		head : headers,
-		colWidths : [20,5,20]
-	})
-	for(var i = 0; i < metaData.length ; i++){
-		var content = [];
-		for(var j = 0; j < keys.length ; j ++){
-			content.push(metaData[i][keys[j]]);
-		}
-		table.push(content);
-	}
-	callback(table);
-}
-
-function AnswerResponse(data,callback){
-	var answer = data[0].expression;
-	callback(answer);
-}
-*/
 
 if(args.q){
 	callSUSI(args.q,function(err,data){
