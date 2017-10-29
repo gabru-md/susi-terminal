13171898c01d0605c1a2cc38833793f262d8e5bf
Mohit Sharma <msvbs98@gmail.com>
2017-10-29 17:31:20 +0530

    Seperate file for description

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
 module.exports = {
-	musicfolder : "YOUR MUSIC FOLDER HERE"
+	musicfolder : "~/Music"
 }
--- src/susi.js
+++ src/susi.js
@@ -4,7 +4,7 @@ const request = require('request');
 const chalk = require('chalk');
 const Table = require('cli-table');
 const path = require('path');
-const figlet = require('figlet');
+const description = require('./description');
 const args = require('./parser');
 const responses = require('./responses');
 const setConfig = require('./setConfig');
@@ -72,19 +72,5 @@ else if(args.play){
 	music.playSong(args.play);
 }
 else{
-    var banner = "SUSI.AI";
-    figlet(banner,{font:"Standard"},function(err,data){
-		if(err){
-			return console.log(chalk.red(err));
-		}
-		else{
-			console.log(chalk.blue(data));
-			console.log(chalk.yellow('\tMeet SUSI.AI, Your Artificial Intelligence for Personal Assistants, Robots, Help Desks and Chatbots.'));
-			console.log(chalk.cyanBright.bold("\n\tAsk it questions. Tell it to do things. Always ready to help."));
-			console.log('\n\tSUSI.AI is available for any android, iOS device and also you can access the web chat application from this URL https://chat.susi.ai\n');
-			console.log(chalk.yellowBright.bold('\tSUSI is having many skills. You can look at the collection of skills at https://skills.susi.ai.'));
-			console.log(chalk.blueBright.bold('\n\tSUSI.AI is Open Source. The code is always available for security reviews and can be improved by anyone with the knowledge and understanding online.'));
-			console.log(chalk.magentaBright.bold('\n\tType susi -h for help.Have a great Experience :)\n'));
-		}
-	});
+    description.Description();
 }
